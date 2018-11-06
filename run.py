121f0e4741e47b7c5c9166cd9e795aa5cbd42e3a
ladm2110 <ladm2110@gmail.com>
2018-11-06 14:58:22 -0500

    Changed output excel

--- run.py
+++ run.py
@@ -11,11 +11,13 @@ log = logging.getLogger(__name__)
 schedule = xml_to_dataframe(sys.argv[1], sys.argv[2], sys.argv[3])
 general_constraints = read_general_constraints(sys.argv[4])
 teacher_constraints = read_teacher_constraints(sys.argv[5])
+time_to_run = int(sys.argv[6])
+
 # Create the mzn file
 write_mzn_file(schedule, general_constraints, teacher_constraints)
 
 days_of_the_week = {0: 'Lunes', 1: 'Martes', 2: 'Miercoles', 3: 'Jueves', 4: 'Viernes'}
-solution = pymzn.minizinc('test_file.mzn', data={'turnos': 30, }, timeout=600)
+solution = pymzn.minizinc('test_file.mzn', data={'turnos': 30, }, timeout=time_to_run)
 solved_ids_bitmap = solution[0]
 
 print('GROUPS SCHEDULES')
@@ -33,7 +35,9 @@ for key, bit_map in solved_ids_bitmap.items():
     pos_in_bitmap = bit_map.index(1)
     day = days_of_the_week[pos_in_bitmap // 6]
     turn = pos_in_bitmap % 6
-    groups_schedule[group_id][day][turn + 1] = subject + '_' + order
+    groups_schedule[group_id][day][turn + 1] = subject + ' (' + schedule.loc[(schedule['Grupo'] == group_id) &
+                                                                             (schedule['Asignatura'] == subject) &
+                                                                             (schedule['Orden'] == order)].Tipo.values[0] + ')'
 
 for group, group_schedule in groups_schedule.items():
     group_schedule.to_excel('output/' + str(group) + '.xlsx')
